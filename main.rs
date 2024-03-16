7de0a6232b582d77a4ecb05b2fed2f1266f5fa2e
Hunnter123 <119716926+Hunnter123@users.noreply.github.com>
2024-03-17 08:42:32 +1100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,5 +1,4 @@
 fn input() -> String {
-    println!("Enter an int:");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let input = input.trim().to_string();
